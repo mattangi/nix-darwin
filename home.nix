959dee9106c6346944553e215d3ea5540995cbbb
mattangi <mattangi@gmail.com>
2025-05-09 00:52:00 +0900

    nix version 25.01

--- home.nix
+++ home.nix
@@ -20,7 +20,8 @@ let
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.11";
+  #home.stateVersion = "24.11";
+  home.stateVersion = "25.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
